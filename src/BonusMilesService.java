95773088c4318d83da94f6516b540bfaf335837e
SergeyESh <SergeyESh1989@yandex.ru>
2022-08-10 20:05:52 +0300

    First commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,8 @@
+ppublic class BonusMilesService {
+    public int calculate(int price) {
+        int cost;
+        int mile = 20; // Количество рублей для одной бонусной мили
+        cost = price / mile; // Количество бонусных миль
+        return cost;
+    }
+}
