06bc9c5fc7e270c463745e73eaa2951406c30d3a
SergeyESh <SergeyESh1989@yandex.ru>
2022-08-19 14:46:02 +0300

    Fixed BonusMilesService

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,4 +1,4 @@
-ppublic class BonusMilesService {
+public class BonusMilesService {
     public int calculate(int price) {
         int cost;
         int mile = 20; // Количество рублей для одной бонусной мили
